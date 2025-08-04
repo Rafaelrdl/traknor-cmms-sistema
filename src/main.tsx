0bb8eed8bb408dad85b493e01864f92d472c14f3
Rafael Ribeiro <rafael@ascitech.com.br>
2025-08-04 23:55:47 +0000

    feat: Adicionar arquivo package.json e configurar dependências iniciais

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+// CRÍTICO: Estes imports DEVEM ser os PRIMEIROS - antes até do React
+import './utils/patchFetchPatch';  // NOVO - Mais agressivo
+import './utils/interceptRequests';
+import './utils/patchTanStackQuery';
+
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
 import "@github/spark/spark"
@@ -5,8 +10,6 @@ import "@github/spark/spark"
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 
-import "./main.css"
-import "./styles/theme.css"
 import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
