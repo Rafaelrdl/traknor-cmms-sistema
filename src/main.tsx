00111cf6541d074011908b407638f3f181857fdb
Rafael Ribeiro <rafael@ascitech.com.br>
2025-08-05 01:26:33 +0000

    feat: adicionar novo arquivo de estilos e otimizar importações do Tailwind CSS

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,7 @@ import "@github/spark/spark"
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 
-import "./main.css"
-import "./styles/theme.css"
-import "./main.css"
+import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
