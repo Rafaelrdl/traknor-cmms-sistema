8f222fd8b5b84133bcacfa1f6c818755eba70c14
Rafael Ribeiro <rafael@ascitech.com.br>
2025-08-05 23:54:17 +0000

    fix: remover código duplicado e organizar importações em main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
-import { ErrorBoundary } from "react-error-boundary";
 import "@github/spark/spark"
+import App from './App.tsx'
 
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
 
 import "./styles/theme.css"
 
-createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <App />
-   </ErrorBoundary>
-)
+
+
+
+
+
