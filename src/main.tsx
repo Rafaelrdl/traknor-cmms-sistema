dd0a0873d662de66455a0ba4ccb9b9ae520bc891
Rafael Ribeiro <rafael@ascitech.com.br>
2025-08-06 00:16:03 +0000

    Generated by Spark: Fix all reported errors.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { ErrorBoundary } from 'react-error-boundary'
 import "@github/spark/spark"
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
-import "./styles/theme.css"
 import "./index.css"
 
 const root = createRoot(document.getElementById('root')!)
